5cad0dae47daa7825cb3cf56ad64048c89729068
MobsofFools <32505517+MobsofFools@users.noreply.github.com>
2022-03-22 11:48:59 -0600

    added read receipt bubble

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { Box } from '@material-ui/core';
+import Avatar from '@material-ui/core/Avatar';
 import { SenderBubble, OtherUserBubble } from '.';
 import moment from 'moment';
 
+const useStyles = makeStyles(() => ({
+  readStatusBubbleContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-end',
+  },
+  unreadStatusBubble: {
+    marginTop: 9,
+    border: '1px solid black',
+    borderRadius: '50%',
+    height: 20,
+    width: 20,
+  },
+  avatar: {
+    height: 20,
+    width: 20,
+    marginTop: 9,
+  },
+}));
 const Messages = (props) => {
+  const classes = useStyles();
   const { messages, otherUser, userId } = props;
+
+  const lastMessageInConversation = () => {
+    if (messages.length > 0) {
+      if (messages[messages.length - 1].senderId === userId) {
+        if (messages[messages.length - 1].readStatus === true) {
+          return { isUsers: true, isRead: true };
+        }
+        return { isUsers: true, isRead: false };
+      } else {
+        return { isUsers: false, isRead: false };
+      }
+    }
+    return;
+  };
 
   return (
     <Box>
@@ -22,6 +58,21 @@ const Messages = (props) => {
           />
         );
       })}
+      {lastMessageInConversation() && lastMessageInConversation().isUsers ? (
+        lastMessageInConversation().isRead ? (
+          <Box className={classes.readStatusBubbleContainer}>
+            <Avatar
+              alt={otherUser.username}
+              src={otherUser.photoUrl}
+              className={classes.avatar}
+            />
+          </Box>
+        ) : (
+          <Box className={classes.readStatusBubbleContainer}>
+            <Box className={classes.unreadStatusBubble}></Box>
+          </Box>
+        )
+      ) : null}
     </Box>
   );
 };
