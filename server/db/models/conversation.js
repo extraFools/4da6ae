bed4dc505858a8b87012e5c2bfa38414178c9fde
MobsofFools <32505517+MobsofFools@users.noreply.github.com>
2022-03-24 17:39:16 -0600

    Changes to models to facilitate group chatting

--- server/db/models/conversation.js
+++ server/db/models/conversation.js
@@ -1,25 +1,45 @@
-const { Op } = require("sequelize");
+const { Op, Sequelize } = require("sequelize");
 const db = require("../db");
 const Message = require("./message");
 
-const Conversation = db.define("conversation", {});
+const Conversation = db.define("conversation", {
+  converstionId: {
+    type: Sequelize.INTEGER,
+    allowNull: false,   
+    unique: true,
+  },
+  conversationTitle: {
+    type: Sequelize.STRING,
+    allowNull: true,
+  }
+});
+
+//Find conversation given conversation Id 
+// Conversation.findConversation = async function (converationId) {
+//   const conversation = await Conversation.findConversation({
+//     where:{
+//       converationId: converationId
+//     }
+//   });
+//   return conversation;
+// }
 
 // find conversation given two user Ids
 
-Conversation.findConversation = async function (user1Id, user2Id) {
-  const conversation = await Conversation.findOne({
-    where: {
-      user1Id: {
-        [Op.or]: [user1Id, user2Id]
-      },
-      user2Id: {
-        [Op.or]: [user1Id, user2Id]
-      }
-    }
-  });
+// Conversation.findConversation = async function (user1Id, user2Id) {
+//   const conversation = await Conversation.findOne({
+//     where: {
+//       user1Id: {
+//         [Op.or]: [user1Id, user2Id]
+//       },
+//       user2Id: {
+//         [Op.or]: [user1Id, user2Id]
+//       }
+//     }
+//   });
 
-  // return conversation or null if it doesn't exist
-  return conversation;
-};
+//   // return conversation or null if it doesn't exist
+//   return conversation;
+// };
 
 module.exports = Conversation;
--- server/db/models/message.js
+++ server/db/models/message.js
@@ -12,4 +12,22 @@ const Message = db.define("message", {
   },
 });
 
+const NewMessage = db.define("message", {
+    text: {
+      type: Sequelize.STRING,
+      allowNull: false,
+    },
+    senderId: {
+      type: Sequelize.INTEGER, // Or guid string
+      allowNull: false
+    },
+    conversationId: {
+      type: Sequelize.INTEGER, // or guid,
+      allowNull: false
+    },
+    viewedBy: {
+      type: Sequelize.ARRAY(Sequelize.INTEGER),
+      allowNull:true,
+    }
+})
 module.exports = Message;
