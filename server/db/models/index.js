369d3b238d0d9aed5f0b710bde4f1f51c2b82bf4
MobsofFools <32505517+MobsofFools@users.noreply.github.com>
2022-03-29 23:21:26 -0600

    changed to belongstomany with a through table

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,10 +4,10 @@ const Message = require("./message");
 
 // associations
 
-User.hasMany(Conversation, {foreignKey:'conversation_id'});
 // Conversation.belongsTo(User, { as: "user1" });
 // Conversation.belongsTo(User, { as: "user2" });
-Conversation.hasMany(User, {foreignKey:'user_id'});
+User.belongsToMany(Conversation, {through: "User_Conversation"});
+Conversation.belongsToMany(User, {through:"User_Conversation"});
 Message.belongsTo(Conversation);
 Conversation.hasMany(Message);
 
